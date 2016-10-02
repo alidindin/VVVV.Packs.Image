1869f91f70190174bae0f0a1b622a9d54ca1dcab
mino <minoru0218@gamil.com>
2016-10-03 00:29:56 +0900

    build with emgucv 2.4.10

--- src/nodes/plugins/Image/OpenCV/src/Calibration/CalibrateCamera.cs
+++ src/nodes/plugins/Image/OpenCV/src/Calibration/CalibrateCamera.cs
@@ -137,7 +137,7 @@ namespace VVVV.CV.Nodes.Calibration
 
 				try
 				{
-					FPinOutError[0] = CameraCalibration.CalibrateCamera(objectPoints, imagePoints, imageSize, intrinsicParam, flags, out extrinsicsPerView);
+					FPinOutError[0] = CameraCalibration.CalibrateCamera(objectPoints, imagePoints, imageSize, intrinsicParam, flags, new MCvTermCriteria(), out extrinsicsPerView);
 
 					Intrinsics intrinsics = new Intrinsics(intrinsicParam, imageSize);
 					FPinOutIntrinsics[0] = intrinsics;
